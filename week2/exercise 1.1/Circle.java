6ca6f9a635b45bb9d5bdccf97ae19ea8455cca62
tranmanhlinh <manhlinh.sama@gmail.com>
2017-11-21 22:37:54 +0700

    Add week2 directory in to repository, done exercise 1.1 of week2

---
+++ week2/exercise 1.1/Circle.java
@@ -0,0 +1,42 @@
+public class Circle1 {
+
+    private double radius;
+    private String color;
+
+    public Circle() {
+        radius = 1.0;
+        color = "red";
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+        color = "red";
+    }
+
+    public Circle(double radius, String color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    // A public method for computing the area of circle
+    public double getArea() {
+        return radius*radius*Math.PI;
+    }
+}
---
+++ week2/exercise 1.1/CircleTest.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CircleTest {
+    Circle circle1 = new Circle(2.0);
+    Circle circle2 = new Circle();
+    Circle circle3 = new Circle(2.2, "blue");
+    Circle circle4 = new Circle();
+    @Test
+    public void getRadius() throws Exception {
+        assertEquals(2.0, circle1.getRadius(), 0.000001);
+        assertEquals(1.0, circle2.getRadius(), 0.00001);
+        assertEquals(2.2, circle3.getRadius(), 0.00001);
+        circle4.setRadius(3);
+        assertEquals(3.0, circle4.getRadius(), 0.00001);
+    }
+
+    @Test
+    public void getColor() throws Exception {
+        assertEquals("red", circle1.getColor());
+        assertEquals("red", circle2.getColor());
+        assertEquals("blue", circle3.getColor());
+        circle4.setColor("black");
+        assertEquals("black", circle4.getColor());
+    }
+
+    @Test
+    public void getArea() throws Exception {
+        assertEquals(Math.PI*2*2, circle1.getArea(), 0.000001);
+        assertEquals(Math.PI, circle2.getArea(), 0.000001);
+        assertEquals(Math.PI*2.2*2.2, circle3.getArea(), 0.00001);
+    }
+
+}
